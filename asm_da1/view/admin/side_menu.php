440dca0c2991009a89628af9e4903769ee63bcf3
ninhadmit147 <59793130+ninhadmit147@users.noreply.github.com>
2022-12-01 23:50:31 +0700

    Add btn sign out

--- asm_da1/view/admin/side_menu.php
+++ asm_da1/view/admin/side_menu.php
@@ -96,8 +96,8 @@
         </a>
     </div>
     <div class="py-[20px] px-[5px]">
-        <p class="text-sm text-gray-500 font-semibold">Insights</p>
-        <a href="./index.php?act=home">
+
+        <a href="../../controller/signout.php">
             <button class="w-full">
                 <div class="py-[10px]">
                     <div
@@ -106,48 +106,11 @@
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                 stroke="currentColor" class="w-6 h-6">
                                 <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
+                                    d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
                             </svg>
 
                         </div>
-                        <p class="font-semibold">Inbox</p>
-                    </div>
-                </div>
-            </button>
-        </a>
-        <a href="./index.php?act=home">
-            <button class="w-full">
-                <div class="py-[10px]">
-                    <div
-                        class="flex items-center text-gray-500 hover:text-white border-0 rounded-2xl hover:bg-[#6C5DD3] px-[12px] duration-300">
-                        <div class="pr-[15px] py-[15px]">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
-                            </svg>
-
-                        </div>
-                        <p class="font-semibold">Notification</p>
-                    </div>
-                </div>
-            </button>
-        </a>
-        <a href="./index.php?act=home">
-            <button class="w-full">
-                <div class="py-[10px]">
-                    <div
-                        class="flex items-center text-gray-500 hover:text-white border-0 rounded-2xl hover:bg-[#6C5DD3] px-[12px] duration-300">
-                        <div class="pr-[15px] py-[15px]">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
-                            </svg>
-
-
-                        </div>
-                        <p class="font-semibold">Comments</p>
+                        <p class="font-semibold">Sign out</p>
                     </div>
                 </div>
             </button>
